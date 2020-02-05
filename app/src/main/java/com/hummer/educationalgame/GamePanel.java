d866074b71826f8ab834a6c07e38c19dff24833d
Fahim Mohammed <mohammedf2606@gmail.com>
2020-02-05 12:15:59 +0000

    Create a rectangle which follows user's cursor/finger

--- app/src/main/java/com/hummer/educationalgame/GamePanel.java
+++ app/src/main/java/com/hummer/educationalgame/GamePanel.java
@@ -2,6 +2,9 @@ package com.hummer.educationalgame;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Point;
+import android.graphics.Rect;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -9,12 +12,18 @@ import android.view.SurfaceView;
 class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
     private MainThread thread;
 
+    private RectPlayer player;
+    private Point playerPoint;
+
     public GamePanel(Context context) {
         super(context);
 
         getHolder().addCallback(this);
 
         thread = new MainThread(getHolder(), this);
+
+        player = new RectPlayer(new Rect(100, 100, 200, 200), Color.rgb(255, 0, 0));
+        playerPoint = new Point(150, 150);
 
         setFocusable(true);
     }
@@ -46,16 +55,28 @@ class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        return super.onTouchEvent(event);
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+            case MotionEvent.ACTION_MOVE:
+                playerPoint.set((int) event.getX(), (int) event.getY());
+        }
+
+
+        return true;
+        //return super.onTouchEvent(event);
     }
 
     public void update() {
-
+        player.update(playerPoint);
     }
 
     @Override
     public void draw(Canvas canvas) {
         super.draw(canvas);
+
+        canvas.drawColor(Color.WHITE);
+
+        player.draw(canvas);
     }
 
 
---
+++ app/src/main/java/com/hummer/educationalgame/RectPlayer.java
@@ -0,0 +1,34 @@
+package com.hummer.educationalgame;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.graphics.Rect;
+
+public class RectPlayer implements GameObject {
+
+    private Rect rectangle;
+    private int colour;
+
+    public RectPlayer(Rect rectangle, int colour) {
+        this.rectangle = rectangle;
+        this.colour = colour;
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        Paint paint = new Paint();
+        paint.setColor(colour);
+        canvas.drawRect(rectangle, paint);
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    public void update(Point point) {
+        rectangle.set(point.x - rectangle.width()/2, point.y - rectangle.height()/2,
+                point.x + rectangle.width()/2, point.y + rectangle.height()/2);
+    }
+}
